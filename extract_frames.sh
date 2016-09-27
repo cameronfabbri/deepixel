c31ded0ede1606f79e7967188162bf9c1e6937d1
Cameron Fabbri <cameronfabbri@gmail.com>
2016-09-26 21:48:14 -0400

    changed script from windows to linux

--- extract_frames.sh
+++ extract_frames.sh
@@ -4,5 +4,5 @@ for file in "$1"/*.*; do
    destination="${file%.*}"
    echo "Extracting from $file..."
    mkdir -p "$destination"
-   ./ffmpeg_g.exe -i "$file" -r 1/1 "$destination/image_%03d.bmp"
+   ffmpeg -i "$file" -r 1/1 "$destination/image_%03d.bmp"
 done
