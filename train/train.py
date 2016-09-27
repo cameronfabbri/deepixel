b1ce7e51558ad9c07be434aa9accbb4a96537d26
Cameron Fabbri <cameronfabbri@gmail.com>
2016-09-27 06:51:55 -0400

    small change to train

--- train/train.py
+++ train/train.py
@@ -5,25 +5,18 @@ import sys
 import numpy as np
 import cv2
 
-sys.path.insert(0, '../utils/')
 sys.path.insert(0, '../inputs/')
 sys.path.insert(0, '../model/')
 
-import config
 import input_
 import architecture
 import time
-
-checkpoint_dir = config.checkpoint_dir
-batch_size = config.batch_size
-
 
 def train():
    with tf.Graph().as_default():
       global_step = tf.Variable(0, trainable=False)
 
       images = input_.inputs("train", batch_size)
-      #images = tf.div(images, 255)
 
       logits = architecture.inference(images, "train")
 
@@ -61,29 +54,17 @@ def train():
          print "Step: " + str(step) + " Loss: " + str(loss_value)
 
          # save for tensorboard
-         if step%5000 == 0 and step != 0:
+         if step%(train_size/batch_size) == 0 and step != 0:
             summary_str = sess.run(summary_op)
             summary_writer.add_summary(summary_str, step)
 
             print "Saving model..."
             saver.save(sess, checkpoint_dir+"training", global_step=step)
-            """
-            c = 1
-            for im, gen in zip(imgs, generated_image):
-               im = np.uint8(im)
-               gen = np.uint8(gen)
-               cv2.imshow('img', im)
-               cv2.imshow('gen', gen)
-               #cv2.waitKey(0)
-               time.sleep(5)
-               cv2.destroyAllWindows()
-               if c == 3:
-                  break
-               c += 1
-            """
             
 
 def main(argv=None):
+
+
    train()
 
 
