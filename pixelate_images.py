e908db1d860f8905e1fa513df199a6dcabb2cb71
Cameron Fabbri <cameronfabbri@gmail.com>
2016-09-28 18:40:30 -0400

    removed black lines

--- pixelate_images.py
+++ pixelate_images.py
@@ -2,17 +2,17 @@ from PIL import Image
 
 backgroundColor = (0,)*3
 #pixelSize = 9
-pixelSize = 15
+pixelSize = 13
 
 image = Image.open('input.png')
 image = image.resize((image.size[0]/pixelSize, image.size[1]/pixelSize), Image.NEAREST)
 image = image.resize((image.size[0]*pixelSize, image.size[1]*pixelSize), Image.NEAREST)
 pixel = image.load()
 
-for i in range(0,image.size[0],pixelSize):
-  for j in range(0,image.size[1],pixelSize):
-    for r in range(pixelSize):
-      pixel[i+r,j] = backgroundColor
-      pixel[i,j+r] = backgroundColor
+#for i in range(0,image.size[0],pixelSize):
+#  for j in range(0,image.size[1],pixelSize):
+#    for r in range(pixelSize):
+#      pixel[i+r,j] = backgroundColor
+#      pixel[i,j+r] = backgroundColor
 
 image.save('output.png')
