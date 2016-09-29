b29c2d48dbfd4e5d0105e84be9e7bc11354e336b
Cameron Fabbri <cameronfabbri@gmail.com>
2016-09-28 21:04:31 -0400

    example images

--- scripts/pixelate_images.py
+++ scripts/pixelate_images.py
@@ -6,7 +6,8 @@ pixelSizes = range(1,20)
 
 for pixelSize in pixelSizes:
    image = Image.open(sys.argv[1])
+   image.save('../images/output-original.png')
    image = image.resize((image.size[0]/pixelSize, image.size[1]/pixelSize), Image.NEAREST)
    image = image.resize((image.size[0]*pixelSize, image.size[1]*pixelSize), Image.NEAREST)
    pixel = image.load()
-   image.save('output-'+str(pixelSize)+'.png')
+   image.save('../images/output-'+str(pixelSize)+'.png')
