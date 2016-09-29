ec5646de1325c51461e84e55e33c4aaa173d61a1
Cameron Fabbri <cameronfabbri@gmail.com>
2016-09-28 20:50:20 -0400

    moved scripts

--- extract_frames.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in "$1"/*.*; do
-   destination="${file%.*}"
-   echo "Extracting from $file..."
-   mkdir -p "$destination"
-   ffmpeg -i "$file" -r 1/1 "$destination/image_%03d.bmp"
-done
