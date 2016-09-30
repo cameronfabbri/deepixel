6d298c457b966a82228e8f00e2613ab5516f0ee7
Cameron Fabbri <cameronfabbri@gmail.com>
2016-09-29 20:36:00 -0400

    changed image type because of space issues

--- scripts/extract_frames.sh
+++ scripts/extract_frames.sh
@@ -4,5 +4,5 @@ for file in "$1"/*.*; do
    destination="${file%.*}"
    echo "Extracting from $file..."
    mkdir -p "$destination"
-   ffmpeg -i "$file" -r 1/1 "$destination/image_%03d.bmp"
+   ffmpeg -i "$file" -r 1/1 "$destination/image_%03d.png"
 done
