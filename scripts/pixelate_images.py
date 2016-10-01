71c6323e851767e1969360b9d498cda54826db84
Cameron Fabbri <cameronfabbri@gmail.com>
2016-10-01 15:29:44 -0400

    minor modifications

--- scripts/pixelate_images.py
+++ scripts/pixelate_images.py
@@ -1,11 +1,10 @@
 from PIL import Image
 import sys
 import cv2
+import numpy as np
+pixelSizes = [4,5]
 
-pixelSizes = [3,4]
-#colors = [10,15,20,25,30]
-
-colors = [100]
+colors = [50, 100, 150, 200]
 
 for pixelSize in pixelSizes:
    for color in colors:
@@ -15,6 +14,9 @@ for pixelSize in pixelSizes:
       image = image.resize((image.size[0]*pixelSize, image.size[1]*pixelSize), Image.NEAREST)
       image = image.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=color)
       pixel = image.load()
+      
+      im = np.array(image)
       image.save('../images/output-00'+str(pixelSize)+'-00'+str(color)+'.png')
+
 
 
--- utils/README.txt
+++
@@ -1,2 +0,0 @@
-test train split should be across different shows,
-or at least across different episodes
