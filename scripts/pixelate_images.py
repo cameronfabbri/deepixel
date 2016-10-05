0b453a3c1b0c0fb3b303bd68af7c0a4abc076731
Cameron Fabbri <cameronfabbri@gmail.com>
2016-10-04 22:28:27 -0400

    added some nonworking stuff

--- scripts/pixelate_images.py
+++ scripts/pixelate_images.py
@@ -2,21 +2,36 @@ from PIL import Image
 import sys
 import cv2
 import numpy as np
-pixelSizes = [4,5]
 
-colors = [50, 100, 150, 200]
+#color = 50
+pixelSize = 12
+SHAPE=(160,144)
 
-for pixelSize in pixelSizes:
-   for color in colors:
-      image = Image.open(sys.argv[1])
-      image.save('../images/output-original.png')
-      image = image.resize((image.size[0]/pixelSize, image.size[1]/pixelSize), Image.NEAREST)
-      image = image.resize((image.size[0]*pixelSize, image.size[1]*pixelSize), Image.NEAREST)
-      image = image.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=color)
-      pixel = image.load()
-      
-      im = np.array(image)
-      image.save('../images/output-00'+str(pixelSize)+'-00'+str(color)+'.png')
+# pixelate image
+image = Image.open(sys.argv[1])
+image = image.resize((image.size[0]/pixelSize, image.size[1]/pixelSize), Image.NEAREST)
+image = image.resize((image.size[0]*pixelSize, image.size[1]*pixelSize), Image.NEAREST)
 
+# convert to opencv
+img = np.asarray(image)
+img = img[:,:,::-1].copy()
 
+# resize to gameboy color resolution
+img = cv2.resize(img, SHAPE, interpolation = cv2.INTER_CUBIC)
+
+# convert BACK to PIL so I can apply a color transformation
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = Image.fromarray(img)
+
+img = img.convert(mode='P', colors=16)
+
+# convert BACK to opencv
+img = np.asarray(img)
+img = img[:,:,::-1].copy()
+
+#cv2.imwrite('resize.png', img)
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
--- utils/createRecords.py
+++ utils/createRecords.py
@@ -1,22 +1,46 @@
 from PIL import Image
+import tensorflow as tf
+from tqdm import tqdm
 import sys
 import cv2
 import numpy as np
-pixelSizes = [4,5]
+import os
+import fnmatch
 
+pixelSizes = [4,5]
 colors = [50, 100, 150, 200]
 
-for pixelSize in pixelSizes:
-   for color in colors:
-      image = Image.open(sys.argv[1])
-      image.save('../images/output-original.png')
-      image = image.resize((image.size[0]/pixelSize, image.size[1]/pixelSize), Image.NEAREST)
-      image = image.resize((image.size[0]*pixelSize, image.size[1]*pixelSize), Image.NEAREST)
-      image = image.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=color)
-      pixel = image.load()
+def setup(folder, dest_dir):
+   
+   folder = "/home/neptune/data_dir/games/"
+   pattern = "*.png"
+   fileList = list()
+
+   for d, s, fList in os.walk(folder):
+      for filename in fList:
+         if fnmatch.fnmatch(filename, pattern):
+            fileList.append(os.path.join(d,filename))
+
+   for image_name in fileList:
+      for pixelSize in pixelSizes:
+         for color in colors:
+            image = Image.open(image_name)
+            image = image.resize((image.size[0]/pixelSize, image.size[1]/pixelSize), Image.NEAREST)
+            image = image.resize((image.size[0]*pixelSize, image.size[1]*pixelSize), Image.NEAREST)
+            image = image.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=color)
+            pixel = image.load()
+            
+            im = np.array(image)
       
-      im = np.array(image)
-      image.save('../images/output-00'+str(pixelSize)+'-00'+str(color)+'.png')
 
+if __name__ == "__main__":
 
+   if len(sys.argv) < 3:
+      print "Usage: python createRecords.py [source directory] [destination directory]"
+      exit()
+
+   folder = sys.argv[1]
+   dest_dir  = sys.argv[2]
+
+   setup(folder, dest_dir)
 
